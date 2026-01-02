220be5720ca32c28c1b3e251fc465d6d2857404b
JavierMdata <datajaviermad@gmail.com>
2026-01-02 18:15:44 -0400

    Update supabase.ts

--- lib/supabase.ts
+++ lib/supabase.ts
@@ -10,9 +10,12 @@ import { createClient } from '@supabase/supabase-js';
  * VITE_SUPABASE_ANON_KEY
  */
 
+
 const supabaseUrl = import.meta.env.VITE_SUPABASE_URL || import.meta.env.NEXT_PUBLIC_SUPABASE_URL || '';
 const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY || import.meta.env.NEXT_PUBLIC_SUPABASE_ANON_KEY || '';
 
+// EXPORTAR es la clave
+export const supabase = createClient(supabaseUrl, supabaseAnonKey);
 // Verificamos si las credenciales son válidas
 const isValidConfig = supabaseUrl && supabaseUrl.startsWith('https://') && supabaseAnonKey;
 
